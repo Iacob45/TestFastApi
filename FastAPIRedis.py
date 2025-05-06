6f18f97f4c10ab2fd5aef52b67c2d8a0f362d8e2
Iacob Delcea <iacob.delcea@chronosit.ro>
2025-05-06 11:23:58 +0300

    Finished implementing Redis database and caching for the tools FastApi

--- FastAPIRedis.py
+++ FastAPIRedis.py
@@ -2,13 +2,13 @@ from enum import Enum
 import uvicorn
 from fastapi import FastAPI, HTTPException, Path, Query
 from pydantic import BaseModel, Field
-import redis
+import redis.asyncio as redis
 import httpx
 import json
 
 app = FastAPI(
-    title="Api1",
-    description="Test API using dictionary of tools",
+    title="FastAPI Redis",
+    description="Test API using Redis as database and cache",
     version="0.1"
 )
 
@@ -25,49 +25,115 @@ class Item(BaseModel):
     id: int = Field(description="Unique identifier of the item")
     category: Category = Field(description="Category of the item")
 
+'''
 items = {
     0: Item(name="Hammer", price=9.99, count=20, id=0, category=Category.TOOLS),
-    1: Item(name="APliers", price=3.5, count=5, id=1, category=Category.TOOLS),
+    1: Item(name="Pliers", price=3.5, count=5, id=1, category=Category.TOOLS),
     2: Item(name="Nails", price=5.99, count=4, id=2, category=Category.CONSUMABLES)
 }
+'''
 
 Selection = dict[str,str|int|float|Category|None]
 
+
+async def insert_item(item: Item):
+    key = f"item:{item.id}"
+    value = item.model_dump_json()
+    return await app.state.redis.set(key,value)
+
+async def update_item(item_id,name,price,count,category):
+    key = f"item:{item_id}"
+    item_data = await app.state.redis.get(key)
+    item_data = json.loads(item_data)
+    if name is not None:
+        item_data["name"] = name
+    if price is not None:
+        item_data["price"]= price
+    if count is not None:
+        item_data["name"] = count
+    if category is not None:
+        item_data["category"] = category
+    item = Item(**item_data)
+    value = item.model_dump_json()
+    if await app.state.redis.set(key, value) is True:
+        return item
+    else:
+        return False
+
+
+async def delete_item_by_id(id: int):
+    #return await app.state.redis.delete(f"item:{id}")
+    item = await app.state.redis.execute_command("GETDEL", f"item:{id}")
+    return json.loads(item)
+async def exists_item(item: Item):
+    return await app.state.redis.exists(f"item:{item.id}")
+
+async def exists_id(id: int):
+    return await app.state.redis.exists(f"item:{id}")
+
+async def return_hashes():
+    items = []
+    async for key in app.state.redis.scan_iter(type=str, match="item:*"):
+        value = await app.state.redis.get(key)
+        items.append(json.loads(value))
+    return items
+
+async def return_hashes_by_params(name,price,count,category):
+    items = []
+    item = 0
+    async for key in app.state.redis.scan_iter(type=str, match="item:*"):
+        item = await app.state.redis.get(key)
+        item = json.loads(item)
+        if all(
+            (
+                name is None or item["name"] == name,
+                price is None or item["price"] == price,
+                count is None or item["count"] == count,
+                category is None or item["category"] is category
+            )
+        ) == 1:
+            items.append(item)
+    return items
+
+
+
+async def return_item(id: int):
+    item = await app.state.redis.get(f"item:{id}")
+    return json.loads(item)
+
+
 #GET------------------------------------------------------------------------
 @app.get("/items")
-def index() -> dict[str,dict[int, Item]]:
+async def index() -> dict[str,list[dict]]:
+    items = await return_hashes()
     return {"items":items}
 
 @app.get("/items/{item_id}")
-def query_item_by_id(item_id: int = Path(ge=0)) -> Item:
-    if item_id not in items:
+async def query_item_by_id(item_id: int = Path(ge=0)) -> dict[str,dict]:
+    if await exists_id(item_id) == 0:
         raise HTTPException(status_code=404, detail=f"Item with {item_id=} does not exist.")
-    return items[item_id]
+    item = await return_item(item_id)
+    return {f"item with {item_id=}": item}
+
 
 @app.get("/sth")
 def show_test_string() -> str:
     return "something"
 
 @app.get("/testing")
-def query_all_items() -> dict[int, Item]:
+async def query_all_items() -> list[dict]:
+    items = await return_hashes()
     return items
 
+
 @app.get("/chooseitem")
-def query_item_by_parameters(
+async def query_item_by_parameters(
         name: str | None = None,
         price: float | None = Query(default=None,ge=0),
         count: int | None = Query(default=None,ge=0),
-        category: Category | None = None) -> dict[str, list | Selection]:
-    def check_item(item: Item) -> bool:
-        return all(
-            (
-                name is None or item.name == name,
-                price is None or item.price == price,
-                count is None or item.count == count,
-                category is None or item.category is category
-            )
-        )
-    selection = [item for item in items.values() if check_item(item)]
+        category: Category | None = None) -> dict[str, dict|list]:
+
+    selection = await return_hashes_by_params(name=name,price=price,count=count,category=category)
     return {
         "query": {"name": name, "price": price, "count": count, "category": category},
         "selection": selection
@@ -76,18 +142,20 @@ def query_item_by_parameters(
 
 #POST------------------------------------------------------------------------
 @app.post("/items")
-def add_item(item: Item) -> dict[str, Item]:
+async def add_item(item: Item) -> dict[str, Item]:
 
-    if item.id in items:
+    if await exists_item(item):
         raise HTTPException(status_code=400, detail=f"Item with {item.id=} already exists.")
 
-    items[item.id] = item
-    return {"added":item}
+    if await insert_item(item):
+        return {"added":item}
+    else:
+        raise HTTPException(status_code=400, detail=f"Something happened, item not added.")
 
 
 #PUT------------------------------------------------------------------------
 @app.put("/items/{item_id}")
-def updateoradd(
+async def updateoradd(
         item_id: int = Path(ge=0),
         name: str | None = None,
         price: float | None = Query(default=None,ge=0),
@@ -97,64 +165,56 @@ def updateoradd(
     #    raise HTTPException(status_code=400, detail=f"Item with {item_id=} does not exist.")
     if all(info is None for info in (name, price, count, category)):
         raise HTTPException(status_code=400, detail=f"No parameters provided.")
-    if item_id in items:
-        item = items[item_id]
-        if name is not None:
-            item.name = name
-        if price is not None:
-            item.price = price
-        if count is not None:
-            item.name = count
-        if category is not None:
-            item.category = category
-        return {"updated": item}
+    if await exists_id(item_id):
+        item = await update_item(item_id=item_id, name=name, price=price, count=count, category=category)
+        if item is not None:
+            return {"updated": item}
+        else:
+            raise HTTPException(status_code=400, detail=f"Something happened, item not updated.")
     elif all(info is not None for info in (name, price, count, category)):
-        items[item_id] = Item(name=name, price=price, count=count, id=item_id, category=category)
-        return {"added": items[item_id]}
+        item = Item(name=name, price=price, count=count, id=item_id, category=category)
+        if await insert_item(item):
+            return {"added": item}
+        else:
+            raise HTTPException(status_code=400, detail=f"Something happened, item not added.")
     else:
         raise HTTPException(status_code=400, detail=f"Item with {item_id=} does not exist and not all parameters were added for creating a new Item")
 
 
 #PATCH------------------------------------------------------------------------
-@app.patch("/items/{item_id}",
-           responses={
-               404: {"description": "Item not found"},
-               400: {"description": "No arguments specified"},
-           }
-)
-def update(
+@app.patch("/items/{item_id}")
+async def update(
         item_id: int = Path(ge=0),
         name: str | None = None,
         price: float | None = Query(default=None,ge=0),
         count: int | None = Query(default=None,ge=0),
-        category: Category | None = None) -> dict[str, Item]:
-    if item_id not in items:
+        category: Category | None = None):
+    if await exists_id(item_id) == 0:
         raise HTTPException(status_code=404, detail=f"Item with {item_id=} does not exist.")
     if all(info is None for info in (name, price, count, category)):
         raise HTTPException(status_code=400, detail=f"No parameters provided for update.")
 
-    item = items[item_id]
-    if name is not None:
-        item.name = name
-    if price is not None:
-        item.price = price
-    if count is not None:
-        item.name = count
-    if category is not None:
-        item.category = category
+    item = await update_item(item_id=item_id, name=name, price=price, count=count, category=category)
 
-    return {"updated": item}
+    if item is not None:
+        return {"updated":item}
+    else:
+        raise HTTPException(status_code=400, detail=f"Something happened, item not updated.")
+
 
 
 #DELETE------------------------------------------------------------------------
 @app.delete("/items/{item_id}")
-def delete_item(item_id: int = Path(ge=0)) -> dict[str, Item]:
+async def delete_item(item_id: int = Path(ge=0)) -> dict[str, Item]:
 
-    if item_id not in items:
-        raise HTTPException(status_code=404, detail=f"Item with {item_id=} does not exist.")
+    if await exists_id(item_id) == 0:
+        raise HTTPException(status_code=400, detail=f"The item with {item_id=} doesn't exist.")
 
-    item = items.pop(item_id)
-    return {"deleted": item}
+    item = await delete_item_by_id(item_id)
+    if item is not None:
+        return {"deleted": item}
+    else:
+        raise HTTPException(status_code=400, detail=f"Something happened, item was not deleted.")
 
 
 print("Working")
@@ -166,7 +226,7 @@ print("Working")
 #STEP 2 - CACHING
 @app.on_event("startup")
 async def startup_event():
-    pool = redis.ConnectionPool(host="127.0.0.1", port=int(6379), db=int(0), password="parola divina23^&")
+    pool = redis.ConnectionPool(host="127.0.0.1", port=int(6379), db=int(1), password="parola divina23^&")
     app.state.redis = redis.Redis(connection_pool=pool)
     app.state.http_client = httpx.AsyncClient()
 
